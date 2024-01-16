ad27a33954b6608b2b6646bc3415b8f266b05e5e
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-16 20:41:53 +0630

    handling patch requests

--- app.js
+++ app.js
@@ -38,6 +38,21 @@ app.get('/api/v1/tours/:id', (req, res) => {
     });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: "<Updated tour here...>"
+        }
+    });
+});
+
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body);
     const newId = tours[tours.length - 1].id + 1;
