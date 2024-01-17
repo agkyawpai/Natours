f25557ec523c0f179ad517397b02870148c79256
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-17 23:20:17 +0630

    enviornment variables

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ const userRouter = require('./routes/userRoutes');
 
 app.use(express.json());
 
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 })
 
-app.use(morgan('dev'));
+
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
