93beb0e0f2a68bdde730b4d84d28fe6dcb335704
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-19 21:28:12 +0630

    filtering the api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,14 +2,23 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async(req, res) => {
     try {
-    const tours = await Tour.find();
-    res.status(200).json({
-        status: 'success',  
-        results: tours.length,
-        requestedAt: req.requestTime,
-        data: {
-            tours,
-        }
+        //BUILD QUERY
+        const queryObj = {...req.query};
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
+        const query = Tour.find(queryObj);
+        
+        //EXECUTE QUERY
+        const tours = await query;
+
+        //SEND RESPONSE
+        res.status(200).json({
+            status: 'success',  
+            results: tours.length,
+            requestedAt: req.requestTime,
+            data: {
+                tours,
+            }
     });
 } catch(err) {
     res.status(400).json({
