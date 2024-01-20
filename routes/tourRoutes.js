5ded254ed13f6b95546d4457f8080b6e7bec2275
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-20 21:49:10 +0630

    aggregation pipline: matching and grouping

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,12 +3,15 @@ const tourController = require('../controllers/tourController');
 const router = express.Router();
 
 router
-    .route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+    .route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
+
+router
+    .route('/tour-stats').get(tourController.getTourStats);
 
 router
     .route('/')
     .get(tourController.getAllTours)
-    .post(tourController.createTour)
+    .post(tourController.createTour);
 
 router 
     .route('/:id')
