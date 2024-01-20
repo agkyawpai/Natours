0def771b3fb1225ff25dc92cc586db4a0d6b7ac1
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-20 23:50:29 +0630

    document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration']
@@ -58,9 +60,26 @@ const tourSchema = new mongoose.Schema({
         virtuals: true
     }
 });
+
 tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7;
-})
+});
+
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+// tourSchema.pre('save', function(next) {
+//     console.log("Will save document...");
+//     next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//     console.log(doc);
+//     next();
+// });
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
