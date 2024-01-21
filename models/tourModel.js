cb3d10bedf128f03593bda925c07294c28482957
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-21 11:10:58 +0630

    data validation: bulit-in

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,7 +4,11 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal than 40 characters.'];
+        minlength: [10, 'A tour name must have more or equal than 10 characters.'];
+
     },
     slug: String,
     duration: {
@@ -17,11 +21,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [1, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
         type: Number,
