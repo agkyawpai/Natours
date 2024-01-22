40d7cd9fbda1504095be04422e5b9c2f39d15957
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-23 00:04:24 +0630

    errors outside express: unhandled rejections

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const server = app.listen(port, () => {
 
 process.on('unhandledRejection', err => {
   console.log('Unhandled Rejection. Shutting down...');
-  console.log(err);
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
