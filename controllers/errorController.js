0aa8c3d2b164f3e129985f218276088816160178
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-22 23:37:59 +0630

    handling duplicate DB fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,12 @@ const handleCastErrorDB = err => {
     return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = err => {
+    const value = err.keyValue.name;  //Not the same as it's shown in lecture becuz the api changed i guess
+    const message = `Duplicate field : '${value}'. Please use another value!`;
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -44,6 +50,7 @@ module.exports = (err, req, res, next) => {
         let error = {...err};
 
         if(err.name === 'CastError') error = handleCastErrorDB(error);
+        if(err.code === 11000) error = handleDuplicateFieldsDB(error);
 
         sendErrorProd(error, res);
     }   
