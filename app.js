93080d710d4bfcbdb6d68e7b5923e034b5cfc117
Aung Kyaw Paing <aungkyaw.paing@brycenmyanmar.com.mm>
2024-01-22 15:43:02 +0630

    refactoring error handlers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -20,20 +22,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        messsage: `Can't find ${req.originalUrl} on this server.`
-    });
-});
-
-app.error((err, req, res, next) => {
-    err.statusCode = err.statusCode || '500';
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
-});
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  });
+  
+app.use(globalErrorHandler);
 
 module.exports = app;
