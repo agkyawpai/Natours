040b6a892009ae39ab29d289c0a78cc28bdbec11
Aung Kyaw Paing <aungkyaw.paing@brycenmyanmar.com.mm>
2024-01-22 14:03:05 +0630

    global error handling middleware

--- app.js
+++ app.js
@@ -26,4 +26,14 @@ app.all('*', (req, res, next) => {
     });
 });
 
+app.error((err, req, res, next) => {
+    err.statusCode = err.statusCode || '500';
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+});
+
 module.exports = app;
