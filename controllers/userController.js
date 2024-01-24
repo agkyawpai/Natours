6dc381ecba80dc91f17d493a7432b01785877c7b
Aung Kyaw Paing <aungkyaw.paing@brycenmyanmar.com.mm>
2024-01-24 11:47:45 +0630

    logging in users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,6 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
 exports.createUser = (req, res) => {
     res.status(500).json({
         status: 'fail',  
@@ -5,12 +8,18 @@ exports.createUser = (req, res) => {
     });
 };
 
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'fail',  
-        data: "<This route is not defined yet.>"
-    });
-};
+exports.getAllUsers = catchAsync(async(req, res, next) => {
+    const users = await User.find();
+    
+    res.status(200).json({
+        status: 'success',  
+        results: users.length,
+        requestedAt: req.requestTime,
+        data: {
+            users,
+        }
+});
+});
 
 exports.getUser = (req, res) => {
     res.status(500).json({
