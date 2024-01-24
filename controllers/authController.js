881007eef76b2bc2a2c0e428e58ea06044b744ff
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-25 00:14:01 +0630

    authorization user roles and permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,12 +11,7 @@ const signToken = id => {
 }
 
 exports.signup = catchAsync(async(req, res, next) => {
-    const newUser = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm
-    });
+    const newUser = await User.create(req.body);
 
     const token = signToken(newUser._id);
 
@@ -80,3 +75,12 @@ exports.protect = catchAsync(async(req, res, next) => {
     req.user = currentUser;
     next();
 });
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(new AppError('You do not have permission to perform this function.'), 403);
+        }
+        next();
+    }
+}
