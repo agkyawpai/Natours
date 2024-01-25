b2a367130cb5bf98bfcdc6ce7a013c507a536158
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-25 10:17:55 +0630

    password reset token

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -29,7 +30,9 @@ const userSchema = new mongoose.Schema({
     passwordConfirm: {
         type: String,
         requierd: [true, 'Please confirm your password']
-    }
+    },
+    passwordResetToken: String,
+    passwordResetExpires: Date
 }, {
     toJSON: {
         virtuals: true
@@ -62,6 +65,19 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
     }
     return false;
 };
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+    
+    this.passwordResetExpires = Date.now() + 10 * 6000;
+
+    // console.log({ resetToken }, this.passwordResetToken);
+
+    return resetToken;
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User; 
