1f13da554b9de008afcb82fc3dbcca089d29ee3b
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-26 16:02:34 +0630

    implemented rate limitting

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const rateLimit = require('express-rate-limit');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -16,6 +18,14 @@ app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 })
+
+// Allow 100 requests from the same IP in one hour
+const limitter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour.'
+});
+app.use('/api', limitter);
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
