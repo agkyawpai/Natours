baa0b81570fc5edbd915b9f14443fccbc1334933
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-26 16:18:15 +0630

    limit request body size

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const userRouter = require('./routes/userRoutes');
 // Set security HTTP headers
 app.use(helmet());
 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
