a384a4a48e73b4579ef45600ae23dab9f578055f
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-26 16:24:53 +0630

    comment

--- app.js
+++ app.js
@@ -12,12 +12,16 @@ const userRouter = require('./routes/userRoutes');
 // Set security HTTP headers
 app.use(helmet());
 
+// Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.static(`${__dirname}/public`));
 
+// Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
