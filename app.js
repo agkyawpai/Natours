770c8bffe2cf05c1dcbfb2de73b1eaaad98f1fa4
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-26 16:38:35 +0630

    data sanitization

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const app = express();
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -14,6 +16,13 @@ app.use(helmet());
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 
 // Development logging
