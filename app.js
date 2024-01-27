443c5c1cd9cbed00bc71e1dbf6075c5a60dc53bc
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-27 23:45:02 +0630

    prevent parameter pollution

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
