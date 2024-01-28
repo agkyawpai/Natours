0ccd7b6ce7945e731f3be93575d3e717b5650f9c
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-28 21:23:58 +0630

    modelling tour guides: embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./../models/userModel');
+
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -114,6 +116,13 @@ tourSchema.virtual('durationWeeks').get(function() {
 
 tourSchema.pre('save', function(next) {
     this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+
+tourSchema.pre('save', async function() {
+    const guidesPromises = this.guides.map(async id => await User.findById(id));
+    this.guides = await Promise.all(guidesPromises);
     next();
 });
 
