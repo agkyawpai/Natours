06deeaceb468681a801fcd7b595d85644b0a6a42
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-28 23:34:52 +0630

    create and get reviews

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 // Set security HTTP headers
 app.use(helmet());
@@ -59,6 +60,7 @@ app.use('/api', limitter);
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
