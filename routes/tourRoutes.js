d0ebb747dcd9f158a26a1837b2bc789b7dcc55ff
agkyawpai <aungkyawpaing65@gmail.com>
2024-01-29 22:32:15 +0630

    implemented nested routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 const router = express.Router();
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
     .route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
@@ -22,5 +25,13 @@ router
     .get(tourController.getTour)
     .patch(tourController.updateTour)
     .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
+
+// router
+//     .route('/:tourId/reviews')
+//     .post(
+//         authController.protect,
+//         authController.restrictTo('user'),
+//         reviewController.createReview
+//     )
 
 module.exports = router;
